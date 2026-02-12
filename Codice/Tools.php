4f63dac7a04674c5c222d6f740ca404975a13cd5
Filippo027 <filippomartellossi@gmail.com>
2026-02-12 18:50:12 +0100

    Update Tools.php

--- Codice/Tools.php
+++ Codice/Tools.php
@@ -1,6 +1,6 @@
 <?php
 /*funzione per la generazione casuale dei colori*/ 
-$arrayColori = array("rosso", "verde", "blu", "giallo");
+$arrayColori = array("", "verde", "blu", "giallo");
 
 function getRandomColor($arrayColori){
     $colori_da_Indovinare = array();
@@ -13,12 +13,7 @@ function getRandomColor($arrayColori){
     return $colori_da_Indovinare;
 }
 
-/* funzione di print colori necessaria in fase di sviluppo - temporanea*/
-function printArray($array){
-    for($i = 0; $i < count($array); $i++){
-        echo $array[$i] . " ";
-    }
-}
+
 
 /*funzione che controlla se i colori sono presenti nell'array e se sono nella posizione corretta*/
 function checkGuess($randomColors, $userAnswer) {
@@ -60,12 +55,13 @@ function checkGuess($randomColors, $userAnswer) {
 }
 
 $randomColors = getRandomColor($arrayColori);
-$userGuess = array("rosso", "rosso", "blu", "giallo");
+
 
 $risultato = checkGuess($randomColors, $userGuess);
 echo "Posizioni corrette: " . $risultato['corretti'] . "\n";
 echo "Colori presenti (posizione sbagliata): " . $risultato['presenti'];
 
 ?>
+
 
 
