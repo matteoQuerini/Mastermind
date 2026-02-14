395242f16af258f565b2ad0523e402537fc1b82a
Filippo027 <filippomartellossi@gmail.com>
2026-02-14 09:32:11 +0100

    Refactor login logic to use password hashing
    Utilizzo funzione di hashing per rendere nascosta lapassword

--- Codice/login.php
+++ Codice/login.php
@@ -1,19 +1,19 @@
 <?php
+session_start();
 
-    session_start();
+$usernameInput = $_POST["username"];
+$passwordInput = $_POST["password"];
 
-    $usernameInput =  $_POST["username"];
-    $passwordInput =  $_POST["password"];
+$username = "admin";
 
-    $username = "admin";
-    $password = "1qaz!QAZ";
+$passwordHash = '$2y$10$4isIBh8DoEE6orHs2iMk8O2b6k/Q5Q6/MahpzUhxj9NNqd.iodCfG';
 
-
-    if ($usernameInput == $username && $passwordInput == $password){
-        $_SESSION['loggato'] = true;
-        header("Location: dashboard.php");
-
-    } else {
-        header("Location: index.html");
-    }
+if ($usernameInput === $username && password_verify($passwordInput, $passwordHash)) {
+    $_SESSION['loggato'] = true;
+    header("Location: dashboard.php");
+    exit;
+} else {
+    header("Location: index.html");
+    exit;
+}
 ?>
